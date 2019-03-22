02053523c573468264fef098b256c3febdaed6b1
Daniel Waugh <dannyjwaugh@gmail.com>
2019-03-22 16:58:49 -0700

    update to remote

--- Wifi_Remote/button_matrix/button_matrix.ino
+++ Wifi_Remote/button_matrix/button_matrix.ino
@@ -1,23 +1,18 @@
 // Keyboard Matrix Tutorial Example
 // baldengineer.com
 // CC BY-SA 4.0
-  const int LED_1 = 14;   
-const int LED_2 = 12;       
-const int LED_3 = 13;
-const int LED_4 = 5; 
+const int LED_1 = 12;   
+const int LED_2 = 14;       
+const int LED_3 = 16;
 // JP1 is an input
-byte rows[] = {5,1,3};
+byte rows[] = {3,1};
 const int rowCount = sizeof(rows)/sizeof(rows[0]);
  
-// JP2 and JP3 are outputs
-byte cols[] = {14,12,13};
+byte cols[] = {5,4,13};
 const int colCount = sizeof(cols)/sizeof(cols[0]);
- 
 byte keys[colCount][rowCount];
  
 void setup() {
-    pinMode(4, OUTPUT);
-    digitalWrite(4, HIGH);
  
     for(int x=0; x<rowCount; x++) {
         pinMode(rows[x], INPUT);
@@ -44,35 +39,23 @@ void readMatrix() {
     }
 }
  
-void printMatrix() {
-    for (int rowIndex=0; rowIndex < rowCount; rowIndex++) {
-        if (rowIndex < 10)
-            Serial.print(F("0"));
-        Serial.print(rowIndex); Serial.print(F(": "));
- 
-        for (int colIndex=0; colIndex < colCount; colIndex++) {  
-            Serial.print(keys[colIndex][rowIndex]);
-            if (colIndex < colCount)
-                Serial.print(F(", "));
-        }   
-        Serial.println("");
-    }
-    Serial.println("");
-}
- 
 void loop() {
     readMatrix();
-    if(keys[2][1] == 0){
-      digitalWrite(4, LOW);
-      pinMode(LED_1, OUTPUT);    
-      pinMode(LED_2, INPUT);     
-      pinMode(LED_3, INPUT);      
-      pinMode(LED_4, OUTPUT); 
+    if(keys[2][0] == 0){
 
-      digitalWrite(LED_1, HIGH);
-      digitalWrite(LED_4, LOW);
-      delay(5000);
-      digitalWrite(4, HIGH);
+  pinMode(LED_1, OUTPUT);    
+  pinMode(LED_2, OUTPUT);     
+  pinMode(LED_3, INPUT);      
+
+  digitalWrite(LED_1, HIGH);
+  digitalWrite(LED_2, LOW);
+
+  delay(3000);
+
+  pinMode(LED_1, INPUT);    
+  pinMode(LED_2, INPUT);     
+  pinMode(LED_3, INPUT);      
+
       for(int x=0; x<rowCount; x++) {
         pinMode(rows[x], INPUT);
     }
@@ -80,7 +63,6 @@ void loop() {
     for (int x=0; x<colCount; x++) {
         pinMode(cols[x], INPUT_PULLUP);
     }
-      digitalWrite(4, HIGH);
     }
     delay(1000);
 }
--- Wifi_Remote/charlieplexing_sample/charlieplexing_sample.ino
+++ Wifi_Remote/charlieplexing_sample/charlieplexing_sample.ino
@@ -1,12 +1,8 @@
-const int LED_1 = 14;   
-const int LED_2 = 12;       
-const int LED_3 = 13;
-const int LED_4 = 5; 
+const int LED_1 = 12;   
+const int LED_2 = 14;       
+const int LED_3 = 16;
 
 void setup() {
-  // put your setup code here, to run once:
-  pinMode(4, OUTPUT);
-  pinMode(4, LOW);
 }
 
 void loop() {
@@ -14,91 +10,49 @@ void loop() {
   pinMode(LED_1, OUTPUT);    
   pinMode(LED_2, OUTPUT);     
   pinMode(LED_3, INPUT);      
-  pinMode(LED_4, INPUT); 
 
   digitalWrite(LED_1, HIGH);
   digitalWrite(LED_2, LOW);
-  delayMicroseconds(50);
-
+  delay(500);
 
   pinMode(LED_1, OUTPUT);    
   pinMode(LED_2, INPUT);     
   pinMode(LED_3, OUTPUT);      
-  pinMode(LED_4, INPUT); 
 
   digitalWrite(LED_1, HIGH);
   digitalWrite(LED_3, LOW);
-
-  delayMicroseconds(50);
-
-  pinMode(LED_1, OUTPUT);    
-  pinMode(LED_2, INPUT);     
-  pinMode(LED_3, INPUT);      
-  pinMode(LED_4, OUTPUT); 
-
-  digitalWrite(LED_1, HIGH);
-  digitalWrite(LED_4, LOW);
-
-  delayMicroseconds(50);
+ delay(500);
 
   pinMode(LED_1, OUTPUT);    
   pinMode(LED_2, OUTPUT);     
   pinMode(LED_3, INPUT);      
-  pinMode(LED_4, INPUT); 
 
   digitalWrite(LED_2, HIGH);
   digitalWrite(LED_1, LOW);
-
-  delayMicroseconds(50);
+ delay(500);
 
   pinMode(LED_1, INPUT);    
   pinMode(LED_2, OUTPUT);     
   pinMode(LED_3, OUTPUT);      
-  pinMode(LED_4, INPUT); 
 
   digitalWrite(LED_2, HIGH);
   digitalWrite(LED_3, LOW);
-
-  delayMicroseconds(50);
-
-  pinMode(LED_1, INPUT);    
-  pinMode(LED_2, OUTPUT);     
-  pinMode(LED_3, INPUT);      
-  pinMode(LED_4, OUTPUT); 
-
-  digitalWrite(LED_2, HIGH);
-  digitalWrite(LED_4, LOW);
-
-  delayMicroseconds(50);
+ delay(500);
 
   pinMode(LED_1, OUTPUT);    
   pinMode(LED_2, INPUT);     
   pinMode(LED_3, OUTPUT);      
-  pinMode(LED_4, INPUT); 
 
   digitalWrite(LED_3, HIGH);
   digitalWrite(LED_1, LOW);
-
-  delayMicroseconds(50);
+ delay(500);
 
   pinMode(LED_1, INPUT);    
   pinMode(LED_2, OUTPUT);     
   pinMode(LED_3, OUTPUT);      
-  pinMode(LED_4, INPUT); 
 
   digitalWrite(LED_3, HIGH);
-  digitalWrite(LED_2, LOW); 
-
-  delayMicroseconds(50);
-
-  pinMode(LED_1, INPUT);    
-  pinMode(LED_2, INPUT);     
-  pinMode(LED_3, OUTPUT);      
-  pinMode(LED_4, OUTPUT); 
-
-  digitalWrite(LED_3, HIGH);
-  digitalWrite(LED_4, LOW);
-
-  delayMicroseconds(50);
+  digitalWrite(LED_2, LOW);
+ delay(500);
 
 }
